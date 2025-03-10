fb4ed8ec0018efa76757a783329a592747177894
Ammar Sumadana <muhammadammarsumadana@mail.ugm.ac.id>
2025-03-10 13:46:29 +0700

    first commit

---
+++ resources/views/table.blade.php
@@ -0,0 +1,58 @@
+@extends('layout.template')
+
+@section('content')
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Prodi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Alex</td>
+            <td>1234321</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Bruno</td>
+            <td>2345432</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Xpander</td>
+            <td>3456543</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Brian</td>
+            <td>4567654</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Ferguso</td>
+            <td>5678765</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Pep</td>
+            <td>6789876</td>
+            <td>SIG</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>Ammar</td>
+            <td>7890987</td>
+            <td>SIG</td>
+        </tr>
+    </tbody>
+  </table>
+@endsection
